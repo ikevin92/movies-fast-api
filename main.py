ef7dd8b68403c55b9be1a9116d01a6c3112c89b1
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-04-10 15:31:31 -0500

    fix files

--- main.py
+++ main.py
@@ -51,6 +51,6 @@ def message():
     return HTMLResponse('<h1>¡Hola Mundo!</h1>')
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0",
-                port=int(os.environ.get("PORT", 8000)))
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0",
+#                 port=int(os.environ.get("PORT", 8000)))
