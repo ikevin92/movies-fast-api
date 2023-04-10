b641410ab6b1a85f6070925b7ad8fa679c0a2214
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-04-10 15:24:22 -0500

    import uvicorn

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 
+import os
+
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
@@ -46,3 +49,8 @@ movies = [
 @app.get("/", tags=["Home"])
 def message():
     return HTMLResponse('<h1>¡Hola Mundo!</h1>')
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(os.environ.get("PORT", 8000)))
